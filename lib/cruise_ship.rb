cf0090a66efd7f5c1f74f6fb0bdf85cb148ce5de
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 22:55:31 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,9 +7,9 @@
 # }
 
 def select_winner(passengers)
-  
+  winner = ""
   passengers.each do |room, passenger|
-    if room == suite_a && passenger.start_with?("A")
+    if room == :suite_a && passenger.start_with?("A")
       winner = passenger
     end
 end
